888a58afa1655aaa171c28c0c995874c9a8b9e11
shadow2505 <141756242+shadow2505@users.noreply.github.com>
2023-09-05 10:10:38 -0600

    Update Programa que imprime 10 números.cpp

--- Programa que imprime 10 números..cpp
+++ Programa que imprime 10 números..cpp
@@ -6,17 +6,17 @@ int main() {
 
     do {
 
-        printf("Ingresa el número inicial: ");
+        printf("Ingresa el numero inicial: ");
         scanf("%d", &numeroInicial);
 
-        printf("Los 10 números a partir de %d son:\n", numeroInicial);
+        printf("Los 10 numeros a partir de %d son:\n", numeroInicial);
         for (int i = numeroInicial; i < numeroInicial + 10; i++) 
 		{
             printf("%d\n", i);
         }
 
         // Pregunta al usuario si desea continuar
-        printf("¿Deseas continuar? (s/n): ");
+        printf("Â¿Deseas continuar? (s/n): ");
         scanf(" %c", &deseaContinuar); 
     } while (deseaContinuar == 's' || deseaContinuar == 'S');
 
